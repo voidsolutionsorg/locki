de0d9efba73654048322d6b227d19753cb258e47
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2023-12-07 21:31:06 +0100

    feat!: vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { mdsvex } from 'mdsvex';
 
@@ -25,8 +25,8 @@ const config = {
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
-			precompress: true,
-			strict: true
+			runtime: 'edge',
+			memory: '128'
 		})
 	}
 };
